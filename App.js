2e054ea1389625a90e6fdbf332d68b5409328132
yuudachi13 <t2pupa00@students.oamk.fi>
2024-09-30 19:42:33 +0300

    itemilöytyy

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import itemIdList from './itemList';
+
+// satunnainen itemID
+const randomItemId = itemIdList[Math.floor(Math.random() * itemIdList.length)]
+const URL = `http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=${randomItemId}`
 
 export default function App() {
+  const [itemName, setItemName] = useState('')
+  const [itemImage, setItemImage] = useState('')
+  const [itemPrice, setItemPrice] = useState('')
+
+  useEffect(() => {
+    fetch(URL)
+      .then(response => {
+        // Tarkistetaan, onko vastaus JSON-muodossa
+        if (!response.ok) {
+          console.log(randomItemId)
+          throw new Error('kyseistä itemiä ei löydy' + response.status)
+        }
+        return response.json()
+      })
+      .then((json) => {
+        setItemName(json.item.name)
+        setItemImage(json.item.icon_large)
+        setItemPrice(json.item.current.price)
+      })
+      .catch((error) => {
+        console.log('Virhe haettaessa dataa: ', error)
+      })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Random OSRS Item</Text>
+      <Text>{itemName}</Text>
+      {itemImage ? ( // Tarkistetaan, onko itemImage tyhjö
+        <Image source={{ uri: itemImage }} style={{ width: 50, height: 50 }} />
+      ) : (
+        <Text>Ladataan kuvaa</Text>
+      )}
+      <Text>Hinta: {itemPrice} gp</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
